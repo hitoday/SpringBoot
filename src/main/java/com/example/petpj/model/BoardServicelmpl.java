4b642e75196938e6d7d312ca916cb6495bf8d1c0
hitoday <bh460@naver.com>
2022-08-02 16:55:37 +0900

    0802

---
+++ src/main/java/com/example/petpj/model/BoardServicelmpl.java
@@ -0,0 +1,14 @@
+//package com.example.petpj.model;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class BoardServicelmpl {
+//
+//    public void insertBoard(Board board) {
+//        boardRepo.save(board);
+//    }
+//}
---
+++ src/main/java/com/example/petpj/model/Test.java
@@ -0,0 +1,11 @@
+package com.example.petpj.model;
+
+public class Test {
+
+    private int hp = 50;
+
+    public int Gethp(int hp){
+        this.hp = hp;
+        return hp;
+    }
+}
